6453654a721e0e5f9b02e071f3696717546379d7
Dmitrii Panchuk <dmytropanchuk0@gmail.com>
2022-05-19 22:18:01 +0300

    user

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -48,6 +48,7 @@ const Single = () => {
         </div>
         <div className="bottom">
           <h1 className="title">Table</h1>
+          <h2>Test</h2>
         </div>
       </div>
     </div>
