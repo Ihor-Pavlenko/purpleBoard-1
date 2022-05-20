6893c82d006f006ef45103b02df507bde222f3ab
Dmitrii Panchuk <dmytropanchuk0@gmail.com>
2022-05-21 00:32:59 +0300

    Add router links

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,20 +1,49 @@
-import "./datatable.scss"
-import { DataGrid } from '@material-ui/data-grid';
+import "./datatable.scss";
+import { DataGrid } from "@material-ui/data-grid";
 
-import {userColumns, userRows} from "./datatableSource";
+import { userColumns, userRows } from "./datatableSource";
+import { Link } from "react-router-dom";
 
 const Datatable = () => {
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: () => {
         return (
-            <div className="datatable">
-              <DataGrid
-                rows={userRows}
-                columns={userColumns}
-                pageSize={10}
-                checkboxSelection
-                disableSelectionOnClick
-              />
-            </div>
+          <div className="cellAction">
+            <Link to="/users/test" style={{ textDecoration: "none" }}>
+              <div className="viewButton">View</div>
+            </Link>
+            <div className="deleteButton">Delete</div>
+          </div>
         );
-}
+      },
+    },
+  ];
+
+  return (
+    <div className="datatable">
+      <div className="datatableTitle">
+        Add new user
+        <Link
+          to="/users/new"
+          style={{ textDecoration: "none" }}
+          className="link"
+        >
+          Add new
+        </Link>
+      </div>
+      <DataGrid
+        rows={userRows}
+        columns={userColumns.concat(actionColumn)}
+        pageSize={10}
+        checkboxSelection
+        disableSelectionOnClick
+      />
+    </div>
+  );
+};
 
 export default Datatable;
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -14,7 +14,7 @@ const New = ({ title, inputs }) => {
       <div className="newContainer">
         <Navbar />
         <div className="top">
-          <h1>{title}r</h1>
+          <h1>{title}</h1>
         </div>
         <div className="bottom">
           <div className="left">
